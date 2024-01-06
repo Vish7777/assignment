1ea22855e69a756adb424b6875691d77b87a493a
Bandari <922621@cognizant.com>
2024-01-06 15:19:05 +0530

    code

--- assignment.java
+++ assignment.java
@@ -5,7 +5,7 @@ package day27;
 public class assignment {
 	
 	public static void main(String[] args) {
-		 System.out.println("Welcome to world!, Hello world ");
+		 System.out.println("Welcome to world!, Hello Sakshi");
 	        System.out.println("This is Devops assignment");
 	        System.out.println("This is the example");
 	        System.out.println("We are in feature branch");
