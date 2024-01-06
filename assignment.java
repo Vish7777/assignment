f1ed6d6248ba1a683f45930a2e4b0bceca392c5e
Bandari <922621@cognizant.com>
2024-01-06 15:12:20 +0530

    assignment

---
+++ assignment.java
@@ -0,0 +1,16 @@
+package day27;
+
+
+
+public class assignment {
+	
+	public static void main(String[] args) {
+		 System.out.println("Welcome to world!, Hello world ");
+	        System.out.println("This is Devops assignment");
+	        System.out.println("This is the example");
+	        System.out.println("We are in feature branch");
+	        System.out.println("need to merge with master branch");
+	}
+
+
+}
